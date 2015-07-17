dac3edf0bccd4626287bdc670945543306170356
ddd-fun <laserdev@gmail.com>
2015-07-17 07:28:30 +0200

    added fork

--- src/functional/Parallelism.scala
+++ src/functional/Parallelism.scala
@@ -1,5 +1,7 @@
 package functional
 
+import java.lang.Thread
+
 
 object Parallelism {
 
@@ -12,23 +14,32 @@ object Parallelism {
 
 
 
-  def sum(seq: IndexedSeq[Int]) : Int ={
-    if(seq.size <= 1) seq.headOption getOrElse 0
+  def sum(seq: IndexedSeq[Int]) : Par[Int] ={
+    if(seq.size <= 1) Par.unit( seq.headOption getOrElse 0)
     else{
       val (l,r) = seq.splitAt( seq.size / 2 )
-      sum(l) + sum(r)
+      Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_+_)
     }
   }
 
 
-  case class Par[A](a: =>A) {
+  case class Par[A](a: A)
+
+  object Par {
 
     def unit[A](a: =>A):Par[A] = Par(a)
 
-    def get(par: Par[A]) : A = a
+    def get[A](par: Par[A]) : A = par.a
+
+    def map2[A,B,C](par: Par[A], parb: Par[B])(mapf: (A,B)=>C ) :Par[C] ={
+      Par.unit( mapf( Par.get(par), Par.get(parb) ) )
+    }
+
+    def fork[A](a: => Par[A]):Par[A] ={
+      a
+    }
 
   }
-
 
 
 
