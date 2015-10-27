8be738368f05af24af5c2ea1a0d244ee023ffd2a
volodymyr.lazoryshyn <volodymyr.lazoryshyn@wirecard.com>
2015-10-27 17:07:25 +0100

    try to add trait

--- src/functional/Parsing2v.scala
+++ src/functional/Parsing2v.scala
@@ -6,8 +6,10 @@ object Parsing2v {
 
   def main(args: Array[String]): Unit = {
 
-    println( char('a')("a") )
-    println( or(char('a'), char('b'))("a") )
+    import Reference._
+
+    println(char('a')("a"))
+    println(or(char('a'), char('b'))("a"))
 
 
   }
@@ -15,28 +17,63 @@ object Parsing2v {
   type ParsingError = String
   type Parser[A] = String => Either[ParsingError, A]
 
-
-  def char(c:Char): Parser[Char] = {
-    in => {
-      val chars = in.toCharArray
-       chars match {
-         case Array(_) =>  if( chars(0).equals(c) ) Right[ParsingError, Char](c) else Left[ParsingError, Char]("does not match ")
-         case _ => Left[ParsingError, Char]("does not match")
-       }
-    }
-  }
+  object Reference {
 
 
-  def or[A](p1: Parser[A], p2: Parser[A]) : Parser[A] = {
-    in => {
-      val result = p1(in)
-      result match  {
-        case Right(_) => result;
-        case _ => p2(in)
+    def string(s: String): Parser[String]  = {
+      in => {
+        in match {
+          case s => Right(in)
+          case _=> Left("does not match")
+        }
       }
     }
+
+    def char(c: Char): Parser[Char] = {
+      in => {
+        val chars = in.toCharArray
+        chars match {
+          case Array(_) => if (chars(0).equals(c)) Right[ParsingError, Char](c) else Left[ParsingError, Char]("does not match ")
+          case _ => Left[ParsingError, Char]("does not match")
+        }
+      }
+    }
+
+
+    def or[A](p1: Parser[A], p2: Parser[A]): Parser[A] = {
+      in => {
+        val result = p1(in)
+        result match {
+          case Right(_) => result;
+          case _ => p2(in)
+        }
+      }
+    }
+
+
   }
 
 
 
+
+  trait Parsers[ParsingError, Parser[+_]] { self =>
+
+    def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+
+    //implicit def string(s: String): Parser[String]
+
+    implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
+    implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+    ParserOps[String] = ParserOps(f(a))
+
+    case class ParserOps[A](p: Parser[A]) {
+      def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+
+      def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+
+    }
+
   }
+
+}
