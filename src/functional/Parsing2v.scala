0648558f2c772a21afc24ffdc9b8117d45e0235b
volodymyr.lazoryshyn <volodymyr.lazoryshyn@wirecard.com>
2015-10-27 08:52:38 +0100

    added or

--- src/functional/Parsing2v.scala
+++ src/functional/Parsing2v.scala
@@ -6,13 +6,13 @@ object Parsing2v {
 
   def main(args: Array[String]): Unit = {
 
-      println(char('a').apply("a"))
+    println( char('a')("a") )
+    println( or(char('a'), char('b'))("a") )
 
 
   }
 
   type ParsingError = String
-
   type Parser[A] = String => Either[ParsingError, A]
 
 
@@ -20,12 +20,22 @@ object Parsing2v {
     in => {
       val chars = in.toCharArray
        chars match {
-         case Array(_) =>  if( chars(0).equals(c) ) Right[ParsingError, Char](c) else Left[ParsingError, Char](in + " does not match " + c)
-         case _ => Left[ParsingError, Char](in + " does not match " + c)
+         case Array(_) =>  if( chars(0).equals(c) ) Right[ParsingError, Char](c) else Left[ParsingError, Char]("does not match ")
+         case _ => Left[ParsingError, Char]("does not match")
        }
     }
   }
 
+
+  def or[A](p1: Parser[A], p2: Parser[A]) : Parser[A] = {
+    in => {
+      val result = p1(in)
+      result match  {
+        case Right(_) => result;
+        case _ => p2(in)
+      }
+    }
+  }
 
 
 
